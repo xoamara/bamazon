49522039256cebbc75912ea5c776b88e1d843006
Nixinerix <xoamara2@gmail.com>
2019-01-09 16:30:23 -0800

    Updated README

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,12 +2,13 @@
 //ID, Names, Price only
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("dotenv").config();
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "pa$$word",
+    password: process.env.PASSWORD,
     database: "bamazon"
 });
 
